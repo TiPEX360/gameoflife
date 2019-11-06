dbeec39a6c20ceb7e1cd2935fdce789e26e7d44a
Amber <almrothera@gmail.com>
2019-11-06 16:22:36 +0000

    It has begun

--- pgm.go
+++ pgm.go
@@ -39,6 +39,11 @@ func writePgmImage(p golParams, i ioChans) {
 	}
 
 	// TODO: write a for-loop to receive the world from the distributor when outputting.
+	for y := 0; y < p.imageHeight; y++ {
+		for x := 0; x < p.imageWidth; x++ {
+			world[y][x] = <- i.distributor.inputVal
+		}
+	}
 
 	for y := 0; y < p.imageHeight; y++ {
 		for x := 0; x < p.imageWidth; x++ {
